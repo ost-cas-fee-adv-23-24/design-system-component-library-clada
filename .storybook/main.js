98221eeeee1cec048fb8ebafaa67fe450b23403c
Danijel Malinovic <danijelmalinovic2@gmail.com>
2023-11-01 11:13:23 +0100

    feat: add color specification to tailwind and storybook documentation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,32 @@
 module.exports = {
-  stories: [
-    '../src/stories/**/*.stories.mdx',
-    // "../docs/**/*.stories.mdx",
-    '../src/stories/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        postCss: {
-          implementation: require.resolve('postcss'),
+    stories: [
+        '../src/docs/**/*.mdx',
+        '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        {
+            name: '@storybook/addon-styling',
+            options: {
+                postCss: {
+                    implementation: require.resolve('postcss'),
+                },
+            },
         },
-      },
+    ],
+    docs: {
+        autodocs: 'tag',
     },
-  ],
-  framework: '@storybook/react-webpack5',
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    framework: '@storybook/react-webpack5',
+    typescript: {
+        check: false,
+        checkOptions: {},
+        reactDocgen: 'react-docgen-typescript',
+        reactDocgenTypescriptOptions: {
+            shouldExtractLiteralValuesFromEnum: true,
+            propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+        },
     },
-  },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,49 @@
+import colors from 'tailwindcss/colors';
+import plugin from "tailwindcss/plugin";
+
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   content: [
-    './src/components/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      error: '#FF0000',
+      primary: colors.violet,
+      secondary: colors.pink,
+      base: colors.slate,
+    },
+    spacing: {
+      xxs: '4px',
+      xs: '8px',
+      s: '16px',
+      m: '24px',
+      l: '32px',
+      xl: '48px',
+      xxl: '64px'
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    /**
+     * @description custom tailwind utilities
+     * Inspiration for this approach is taken from https://tailwindcss.com/docs/adding-custom-styles#writing-plugins
+     */
+    plugin(function ({addUtilities, theme}) {
+      addUtilities({
+        '.mb-bg-gradient': {
+          background:  `linear-gradient(90deg, ${theme('colors.secondary.500')} 0%, ${theme('colors.primary.600')} 100%)`
+        },
+        '.mb-bg-gradient-hover': {
+          background: `linear-gradient(90deg, ${theme('colors.secondary.600')} -12.56%, ${theme('colors.primary.500',)} 62.92%)`,
+        },
+        '.mb-bg-gradient-focus': {
+          background: `linear-gradient(90deg, ${theme('colors.secondary.600')} -15.33%, ${theme('colors.primary.500')} 38.87%)`,
+        }
+      })
+    })
+  ],
 }
