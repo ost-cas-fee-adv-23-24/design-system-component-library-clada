fbf14938795356486e299c41b686f88e8b020c37
Danijel Malinovic <danijelmalinovic2@gmail.com>
2023-11-15 22:04:32 +0100

    feat: add transitions and clean up code

--- src/components/tabs/tabs.stories.tsx
+++ src/components/tabs/tabs.stories.tsx
@@ -4,6 +4,7 @@ import { Tabs } from './tabs';
 const meta: Meta<typeof Tabs> = {
 	title: 'Components/Tabs',
 	component: Tabs,
+	tags: ['autodocs'],
 };
 
 type Story = StoryObj<typeof meta>;
@@ -15,13 +16,61 @@ export const Default: Story = {
 		tabs: [
 			{
 				text: 'Deine Mumbles',
+				onClick: () => console.log('clicked tab 1'),
+			},
+			{
+				text: 'Deine Likes',
+				onClick: () => console.log('clicked tab 2'),
+			},
+		],
+	},
+};
+
+export const OneItem: Story = {
+	args: {
+		tabs: [
+			{
+				text: 'Deine Mumbles',
 				onClick: () => console.log('clicked'),
-				selected: true,
+			},
+		],
+	},
+};
+
+export const ThreeItems: Story = {
+	args: {
+		tabs: [
+			{
+				text: 'Deine Mumbles',
+				onClick: () => console.log('clicked'),
 			},
 			{
 				text: 'Deine Likes',
 				onClick: () => console.log('clicked'),
-				selected: false,
+			},
+			{
+				text: 'Deine Follower',
+				onClick: () => console.log('clicked'),
+			},
+		],
+	},
+};
+
+export const PreselectTab: Story = {
+	args: {
+		tabs: [
+			{
+				text: 'Deine Mumbles',
+				onClick: () => console.log('clicked'),
+			},
+			{
+				text: 'Deine Likes',
+				onClick: () => console.log('clicked tab 2'),
+				selected: true,
+			},
+			{
+				text: 'Deine Follower',
+				onClick: () => console.log('clicked tab 3'),
 			},
 		],
 	},
--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -1,48 +1,65 @@
 'use client';
 
-import React, { FC, useState } from 'react';
+import React, { FC, Fragment, useState, useRef } from 'react';
 import { Tab } from '@headlessui/react';
 import classNames from 'classnames';
 import { motion } from 'framer-motion';
 
-interface TabItemProps {
-	text: string;
-	onClick: () => void;
-	selected?: boolean;
-}
-
-export interface TabsProps {
-	tabs: TabItemProps[];
-}
+export type TabsProps = {
+	/**
+	 * Collection of multiple tab items
+	 */
+	tabs: {
+		text: string;
+		onClick: () => void;
+		selected?: boolean;
+	}[];
+};
 
 export const Tabs: FC<TabsProps> = ({ tabs }) => {
-	const [selectedItem, setSelectedItem] = useState<number>(tabs.findIndex((tab: TabItemProps) => tab.selected) ?? 0);
+	const [mouseOverIndex, setMouseOverIndex] = useState<number | undefined>(undefined);
+	const getInitialSelectedItem = () => {
+		const index = tabs?.findIndex((tab) => tab?.selected);
+		return index !== -1 ? index : 0;
+	};
+
+	// we need this uuid for the layoutId of the motion.span element
+	// it sets the correct affiliation, so the state changes will not conflict if multiple tabs components would be used on the same site.
+	const uuid = useRef(crypto.randomUUID());
 
 	return (
 		<Tab.Group
+			defaultIndex={getInitialSelectedItem()}
 			onChange={(index) => {
-				tabs[index].onClick();
-				setSelectedItem(index);
+				tabs[index]?.onClick();
 			}}>
-			<Tab.List className={'inline-block rounded-s bg-base-200 p-[5px]'}>
+			<Tab.List className={'inline-flex gap-xs rounded-s bg-base-200 p-[5px]'}>
 				{tabs.map((item, index) => {
 					const { text } = item;
 
 					return (
-						<Tab
-							key={index}
-							className={classNames('relative gap-s rounded-s mb-font-label-l', {
-								'text-primary-600': index === selectedItem,
-								'text-base-600 duration-300 hover:text-base-800': index !== selectedItem,
-							})}>
-							{selectedItem === index && (
-								<motion.span
-									layoutId="bubble"
-									className="inset-0 absolute z-10 h-[100%] w-[100%] rounded-s bg-white"
-									transition={{ type: 'spring', bounce: 0.2, duration: 0.6 }}
-								/>
+						<Tab as={Fragment} key={index}>
+							{({ selected }) => (
+								<button
+									className={classNames('relative rounded-s outline-none mb-font-label-l', {
+										'text-primary-600': selected,
+										'text-base-600 duration-300 hover:text-base-800': !selected,
+									})}
+									onMouseEnter={() => setMouseOverIndex(index)}
+									onMouseLeave={() => setMouseOverIndex(undefined)}>
+									{selected && (
+										<motion.span
+											layoutId={`bubble-${uuid.current}`}
+											className={classNames('inset-0 absolute z-10 h-[100%] w-[100%] rounded-s bg-white', {
+												'w-[calc(100%+4px)]': Number(mouseOverIndex) > index,
+												'-left-xxs w-[calc(100%+4px)]': Number(mouseOverIndex) < index,
+											})}
+											transition={{ type: 'spring', bounce: 0.2, duration: 0.6 }}
+										/>
+									)}
+									<span className={'relative z-20 inline-block p-xs'}>{text}</span>
+								</button>
 							)}
-							<span className={'relative z-20 inline-block p-xs'}>{text}</span>
 						</Tab>
 					);
 				})}
