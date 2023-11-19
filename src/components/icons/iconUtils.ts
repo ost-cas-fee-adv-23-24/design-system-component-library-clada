8280c874b0e7993575da02ef19498472a563dab3
claudio-OST <144836241+claudio-OST@users.noreply.github.com>
2023-11-19 19:13:17 +0100

    feat: add icon components (#44)
    This pull request introduces a set of icon components to our
    application's UI library.
    
    Each icon component has been integrated into Storybook, allowing for
    easy visualization and testing. This ensures that the icons can be
    reviewed in isolation and in different scenarios, confirming their
    design and functionality.
    
    For consistency and ease of use, default properties such as **size,
    color, and, where applicable, filled**, have been set for each icon.
    These defaults ensure a consistent look and feel across our application
    while allowing flexibility for specific use cases.

---
+++ src/components/icons/iconUtils.ts
@@ -0,0 +1,42 @@
+export const getSizeClass = (size: 's' | 'm' | 'l') => {
+	switch (size) {
+		case 's':
+			return 'w-s h-s';
+		case 'm':
+			return 'w-m h-m';
+		case 'l':
+			return 'w-l h-l';
+		default:
+			return 'w-s h-s';
+	}
+};
+
+export const getColorClass = (color: 'base' | 'white' | 'primary' | 'secondary' | 'error') => {
+	switch (color) {
+		case 'base':
+			return 'fill-base-600';
+		case 'secondary':
+			return 'fill-secondary-600';
+		case 'primary':
+			return 'fill-primary-600';
+		case 'white':
+			return 'fill-white';
+		case 'error':
+			return 'fill-error';
+		default:
+			return 'fill-slate-600';
+	}
+};
+
+export interface IconProps {
+	size: 's' | 'm' | 'l';
+	color: 'base' | 'white' | 'primary' | 'secondary' | 'error';
+}
+
+export interface FillableIconProps extends IconProps {
+	filled: true | false;
+}
+
+export interface RotatableIconProps extends IconProps {
+	direction: 'up' | 'down' | 'left' | 'right';
+}
