c104754ffb96b1ee19c221475b930bfdb50ee65a
Danijel Malinovic <danijelmalinovic2@gmail.com>
2023-11-22 21:24:27 +0100

    feat: add input

---
+++ src/components/input/input-password.tsx
@@ -0,0 +1,21 @@
+import React, { forwardRef, useState } from 'react';
+import { EyeIcon } from '../icons';
+import { Input, InputProps } from './input';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface IInputPasswordProps extends Omit<InputProps, 'icon' | 'type' | 'onIconClick'> {}
+
+// eslint-disable-next-line react/display-name
+export const InputPassword = forwardRef<HTMLInputElement, IInputPasswordProps>(({ ...rest }, ref) => {
+	const [showPassword, setShowPassword] = useState<boolean>(false);
+
+	return (
+		<Input
+			ref={ref}
+			Icon={EyeIcon}
+			onIconClick={() => setShowPassword(!showPassword)}
+			type={showPassword ? 'text' : 'password'}
+			{...rest}
+		/>
+	);
+});
---
+++ src/components/input/input-passwort.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { InputPassword } from './input-password';
+
+const meta: Meta<typeof InputPassword> = {
+	title: 'Components/Input Password',
+	component: InputPassword,
+	tags: ['autodocs'],
+};
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const Default: Story = {
+	args: {
+		label: 'Input',
+		placeholder: 'Placeholder',
+	},
+};
