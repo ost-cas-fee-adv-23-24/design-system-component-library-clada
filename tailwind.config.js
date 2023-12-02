1002f2aeaf0b4780cd4769bd0c0c976f87504926
claudio-OST <144836241+claudio-OST@users.noreply.github.com>
2023-12-02 13:40:04 +0100

    feat: add button (#52)
    **Overview**
    
    This PR introduces the new Button component to our design system. This
    component has been designed to provide a flexible and accessible way to
    handle user interactions across our application. It supports multiple
    variants, sizes, and optional icon integration, ensuring adaptability
    for various UI contexts.
    
    **Features**
    
    - Color Variants: Includes 'base', 'primary', and 'secondary' color
    schemes to align with our design system's color palette.
    - Size Options: Two size variants, 'm' (medium) and 'l' (large), to
    accommodate different UI spaces.
    - Icon Support: Optional integration of icons for more dynamic button
    designs.
    - Accessibility Focused: Built with accessibility in mind, ensuring
    usability for all users.

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
 			error: '#FF0000',
 			primary: colors.violet,
 			secondary: colors.pink,
-			base: colors.slate
+			base: colors.slate,
 		},
 		fontFamily: {
 			default: ['Poppins', 'Arial', 'sans-serif'],
@@ -39,15 +39,6 @@ module.exports = {
 			semiBold: '600',
 			bold: '700',
 		},
-		spacing: {
-			xxs: '4px',
-			xs: '8px',
-			s: '16px',
-			m: '24px',
-			l: '32px',
-			xl: '48px',
-			xxl: '64px',
-		},
 		borderWidth: {
 			1: '1px',
 			2: '2px',
@@ -62,6 +53,17 @@ module.exports = {
 			l: '24px',
 			full: '9999px',
 		},
+		extend: {
+			spacing: {
+				xxs: '4px',
+				xs: '8px',
+				s: '16px',
+				m: '24px',
+				l: '32px',
+				xl: '48px',
+				xxl: '64px',
+			},
+		},
 	},
 	plugins: [
 		/**
@@ -71,16 +73,19 @@ module.exports = {
 		plugin(function ({ addUtilities, theme }) {
 			addUtilities({
 				'.mb-bg-gradient': {
+					transition: 'background 0.3s ease-out',
 					background: `linear-gradient(90deg, ${theme('colors.secondary.500')} 0%, ${theme(
 						'colors.primary.600',
 					)} 100%)`,
 				},
 				'.mb-bg-gradient-hover': {
+					transition: 'background 0.3s ease-out',
 					background: `linear-gradient(90deg, ${theme('colors.secondary.600')} -12.56%, ${theme(
 						'colors.primary.500',
 					)} 62.92%)`,
 				},
-				'.mb-bg-gradient-focus': {
+				'.mb-bg-gradient-active': {
+					transition: 'background 0.3s ease-out',
 					background: `linear-gradient(90deg, ${theme('colors.secondary.600')} -15.33%, ${theme(
 						'colors.primary.500',
 					)} 38.87%)`,
